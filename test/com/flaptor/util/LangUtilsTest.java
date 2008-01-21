3266efbc7ce599acdfb2c7c80c9cf5e444eaabe2
martinmassera <martinmassera@a957e277-6143-0410-b0ff-25ec9014f3a9>
2008-01-21 22:12:49 +0000

    forgot spike's test annotations
    git-svn-id: http://flaptor-util.googlecode.com/svn/trunk@13 a957e277-6143-0410-b0ff-25ec9014f3a9

--- test/com/flaptor/util/LangUtilsTest.java
+++ test/com/flaptor/util/LangUtilsTest.java
@@ -2,6 +2,7 @@ package com.flaptor.util;
 
 public class LangUtilsTest extends TestCase{
 	
+    @TestInfo (testType = TestInfo.TestType.UNIT)
 	public void testIdentifier() {
 		assertEquals("en", LangUtils.identify("The police got a confession from the robber that I caught,and the police used this info to capture the second thief.I got the impression that the police "));
 		assertEquals("es", LangUtils.identify("y entonces llegó el olvido para decirme casi en secreto: no la verás ya más. y respondí con mis ojos de mudo con mis labios de ciego:¿a quién?"));
