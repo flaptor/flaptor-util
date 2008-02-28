a1a35c0c909935652088f716c5d9a1a149baaed1
martinmassera <martinmassera@a957e277-6143-0410-b0ff-25ec9014f3a9>
2008-02-28 22:49:06 +0000

    fixed a bug in rmi code generation that failed when client and server were in different machines
    git-svn-id: http://flaptor-util.googlecode.com/svn/trunk@46 a957e277-6143-0410-b0ff-25ec9014f3a9

--- src/com/flaptor/util/remote/RmiCodeGeneration.java
+++ src/com/flaptor/util/remote/RmiCodeGeneration.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Proxy;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.ServerException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -29,51 +30,29 @@ import com.flaptor.util.Execute;
  */
 public class RmiCodeGeneration {
     private static Logger logger = Logger.getLogger(Execute.whoAmI());
-    private static AtomicInteger classCounter = new AtomicInteger(0);
     
     private static final String THROWABLE_IN_REMOTE_HOST_CODE = "caught throwable in remote host code";
+    private static final Map<String, Class> classMap = new HashMap<String, Class>(); 
     
     /**
      * it gives you an object with the same methods as the original one,
      * but implementing Remote and throwing RemoteException in all its methods. 
      * It can handle exceptions so they are sent all the way to the client (calling) code.
      * 
+     * @param remoteClassName the fully qualified name of the generated remote interface. The returned handler will 
+     * be of this class name. This is important because on the client side, RMI loads this interface (you can generate it
+     * using getRemoteInterface or using reconnectableStub)
+     * @param interfaces the interfaces that the object will expose
      * @param originalHandler
      * @return  
      */
-    public static Remote remoteHandler(final Object originalHandler) {
+    public static Remote remoteHandler( String remoteClassName, Class[] interfaces, final Object originalHandler) {
         try {
             logger.debug("creating remote server for  " + originalHandler);
-            
-            String name = "dynamicallyGeneratedRmiHandler.class" + classCounter.incrementAndGet() + "." + originalHandler.getClass().getCanonicalName();     
-            logger.debug("interface name " + name);
-            
-            ClassPool cp = ClassPool.getDefault();
-            CtClass interf = cp.makeInterface(name);
-            interf.setSuperclass(cp.get(Remote.class.getCanonicalName()));
+            Class handlerInterface = getRemoteInterface(remoteClassName, interfaces);
 
-            List<Method> objectMethods = Arrays.asList(Object.class.getMethods());
-            for (Method method : originalHandler.getClass().getMethods()) {
-                if (objectMethods.contains(method)) {
-                    logger.debug("skipping method from object: " + method);
-                    continue;
-                }
-                logger.debug("processing method " + method.getName());
-                
-                Class retType = method.getReturnType();
-                CtClass newRetType = cp.get(retType.getCanonicalName());
-                Class[] paramTypes = method.getParameterTypes();
-                CtClass[] newParamTypes = new CtClass[paramTypes.length];
-                for (int i = 0; i < paramTypes.length; ++i) newParamTypes[i] = cp.get(paramTypes[i].getCanonicalName());
-
-                CtMethod newMethod = new CtMethod(newRetType, method.getName(),newParamTypes, interf);
-                newMethod.setExceptionTypes(new CtClass[] {cp.get(RemoteException.class.getCanonicalName())});
-                interf.addMethod(newMethod);
-            }
-            Class handlerInterface = interf.toClass();
-            
             logger.debug("constructing method map");
-            final Map<Method, Method> methodMap = getMethodMap(handlerInterface, originalHandler.getClass());
+            final Map<Method, Method> methodMap = getMethodMap(new Class[]{handlerInterface}, originalHandler.getClass());
             
             InvocationHandler handler = new InvocationHandler() {
                 public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
@@ -94,6 +73,43 @@ public class RmiCodeGeneration {
         }
     }
 
+    public static Class getRemoteInterface(String remoteClassName, Class[] interfaces) {
+        try{ 
+            ClassPool cp = ClassPool.getDefault();
+            logger.debug("interface name " + remoteClassName);
+            Class handlerInterface = classMap.get(remoteClassName);
+            if (handlerInterface == null) {
+                CtClass generatedInterf = cp.makeInterface(remoteClassName);
+                generatedInterf.setSuperclass(cp.get(Remote.class.getCanonicalName()));
+                List<Method> objectMethods = Arrays.asList(Object.class.getMethods());
+                for (Class interf : interfaces) {
+                    for (Method method : interf.getMethods()) {
+                        if (objectMethods.contains(method)) {
+                            logger.debug("skipping method from object: " + method);
+                            continue;
+                        }
+                        logger.debug("processing method " + method.getName());
+                        
+                        Class retType = method.getReturnType();
+                        CtClass newRetType = cp.get(retType.getCanonicalName());
+                        Class[] paramTypes = method.getParameterTypes();
+                        CtClass[] newParamTypes = new CtClass[paramTypes.length];
+                        for (int i = 0; i < paramTypes.length; ++i) newParamTypes[i] = cp.get(paramTypes[i].getCanonicalName());
+        
+                        CtMethod newMethod = new CtMethod(newRetType, method.getName(),newParamTypes, generatedInterf);
+                        newMethod.setExceptionTypes(new CtClass[] {cp.get(RemoteException.class.getCanonicalName())});
+                        generatedInterf.addMethod(newMethod);
+                    }
+                }
+                handlerInterface = generatedInterf.toClass();
+                classMap.put(remoteClassName, handlerInterface);
+            }
+            return handlerInterface;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
     /**
      * Given a remote, creates a proxy of the provided interface that calls methods in that remotes
      * If exceptions are thrown in the remote server code, they are thrown through the original interface
@@ -103,9 +119,9 @@ public class RmiCodeGeneration {
      * @param originalInterface
      * @return 
      */
-    public static Object proxy(final Remote remote, Class originalInterface) {
+    public static Object proxy(final Remote remote, Class[] originalInterfaces) {
         logger.debug("constructing reverse method map");
-        final Map<Method, Method> methodMap = getMethodMap(originalInterface, remote.getClass());
+        final Map<Method, Method> methodMap = getMethodMap(originalInterfaces, remote.getClass());
         InvocationHandler handler = new InvocationHandler() {
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                 Method originalMethod = methodMap.get(method);
@@ -124,28 +140,30 @@ public class RmiCodeGeneration {
                 }
             }
         };
-        return Proxy.newProxyInstance(originalInterface.getClassLoader(), new Class[]{originalInterface}, handler);
+        return Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(), originalInterfaces, handler);
     }
     
-    private static Map<Method, Method> getMethodMap(Class c1, Class c2) {
+    private static Map<Method, Method> getMethodMap(Class[] calledClasses, Class originalClass) {
         final Map<Method, Method> methodMap = new HashMap<Method, Method>();
-        for (Method calledMethod : c1.getMethods()) {
-            for (Method originalMethod : c2.getMethods()) {
-                if (calledMethod.getName().equals(originalMethod.getName())) {
-                    Class[] params1 = calledMethod.getParameterTypes();
-                    Class[] params2 = originalMethod.getParameterTypes();
-                    if (params1.length != params2.length) continue;
-                                            
-                    boolean equals = true;
-                    for (int i = 0; i < params1.length; ++i) {
-                        if (!params1[i].getCanonicalName().equals(params2[i].getCanonicalName())) {
-                            equals = false;
-                            break;
+        for (Class calledClass : calledClasses) {
+            for (Method calledMethod : calledClass.getMethods()) {
+                for (Method originalMethod : originalClass.getMethods()) {
+                    if (calledMethod.getName().equals(originalMethod.getName())) {
+                        Class[] params1 = calledMethod.getParameterTypes();
+                        Class[] params2 = originalMethod.getParameterTypes();
+                        if (params1.length != params2.length) continue;
+                                                
+                        boolean equals = true;
+                        for (int i = 0; i < params1.length; ++i) {
+                            if (!params1[i].getCanonicalName().equals(params2[i].getCanonicalName())) {
+                                equals = false;
+                                break;
+                            }
                         }
-                    }
-                    if (equals) {
-                        logger.debug(calledMethod + " ->" + originalMethod);
-                        methodMap.put(calledMethod, originalMethod);
+                        if (equals) {
+                            logger.debug(calledMethod + " ->" + originalMethod);
+                            methodMap.put(calledMethod, originalMethod);
+                        }
                     }
                 }
             }
@@ -163,16 +181,17 @@ public class RmiCodeGeneration {
      * @param policy
      * @return an object that implements objectInterface
      */
-    public static Object reconnectableStub(final Class objectInterface, String context, String host, int port, IRetryPolicy policy) {
-        
+    public static Object reconnectableStub(String remoteClassName, final Class[] interfaces, String context, String host, int port, IRetryPolicy policy) {
+        //need to generate the remote interface because RMI will instantiate it
+        getRemoteInterface(remoteClassName, interfaces);
         final StubbingInvocationHandler stubbingInvocationHandler = new StubbingInvocationHandler();
         final ARmiClientStub myStub = new ARmiClientStub(port, host, context, policy) {
             protected void setRemote(Remote stub) {
-                stubbingInvocationHandler.setObjectProxy(proxy(stub, objectInterface));
+                stubbingInvocationHandler.setObjectProxy(proxy(stub, interfaces));
             }
         };
         stubbingInvocationHandler.setRmiStub(myStub);
-        return Proxy.newProxyInstance(objectInterface.getClassLoader(), new Class[]{objectInterface}, stubbingInvocationHandler);
+        return Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(), interfaces, stubbingInvocationHandler);
     }
 
     private static class StubbingInvocationHandler implements InvocationHandler {
--- test/com/flaptor/util/remote/RmiCodeGenerationTest.java
+++ test/com/flaptor/util/remote/RmiCodeGenerationTest.java
@@ -3,6 +3,7 @@ package com.flaptor.util.remote;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.rmi.Remote;
+import java.util.Arrays;
 
 import com.flaptor.util.Execute;
 import com.flaptor.util.TestCase;
@@ -17,7 +18,10 @@ public class RmiCodeGenerationTest extends TestCase{
 //    @TestInfo(testType = TestInfo.TestType.UNIT, requiresPort = {5000})
     public void testCodeGeneration() throws FileNotFoundException {
         server = new RmiServer(5000);
-        Remote remoteHandler = RmiCodeGeneration.remoteHandler(new MyInterface() {
+        Remote remoteHandler = RmiCodeGeneration.remoteHandler(
+            "remote.MyInterface",
+            new Class[]{MyInterface.class},
+            new MyInterface() {
             public String hello(int i) throws FileNotFoundException {
                 return "hola";
             }
@@ -27,10 +31,10 @@ public class RmiCodeGenerationTest extends TestCase{
 
         Remote remote = RmiUtil.getRemoteService("localhost", 5000, "context");
         
-        myInterface = (MyInterface) RmiCodeGeneration.proxy(remote, MyInterface.class);
+        myInterface = (MyInterface) RmiCodeGeneration.proxy(remote, new Class[]{MyInterface.class});
         assertEquals("hola", myInterface.hello(0));
         
-        myInterface = (MyInterface) RmiCodeGeneration.reconnectableStub(MyInterface.class, "context", "localhost", 5000, new ExponentialFallbackPolicy());
+        myInterface = (MyInterface) RmiCodeGeneration.reconnectableStub("remote.MyInterface", new Class[]{MyInterface.class}, "context", "localhost", 5000, new ExponentialFallbackPolicy());
         assertEquals("hola", myInterface.hello(0));
 
         Execute.stop(server);
@@ -39,7 +43,10 @@ public class RmiCodeGenerationTest extends TestCase{
 //    @TestInfo(testType = TestInfo.TestType.UNIT, requiresPort = {5000})
     public void testException() throws FileNotFoundException {
         server = new RmiServer(5000);
-        Remote remoteHandler = RmiCodeGeneration.remoteHandler(new MyInterface() {
+        Remote remoteHandler = RmiCodeGeneration.remoteHandler(
+            "remote.MyInterface",
+            new Class[]{MyInterface.class},
+            new MyInterface() {
             public String hello(int i) throws FileNotFoundException {
                 throw new FileNotFoundException("my exception!");
             }
@@ -48,7 +55,7 @@ public class RmiCodeGenerationTest extends TestCase{
         server.start();
         
         Remote remote = RmiUtil.getRemoteService("localhost", 5000, "context");
-        myInterface = (MyInterface) RmiCodeGeneration.proxy(remote, MyInterface.class);
+        myInterface = (MyInterface) RmiCodeGeneration.proxy(remote, new Class[]{MyInterface.class});
         try {
             myInterface.hello(0);
             fail("an exception should have occurred");
@@ -56,7 +63,7 @@ public class RmiCodeGenerationTest extends TestCase{
             assertEquals("my exception!", e.getMessage());
         }
 
-        myInterface = (MyInterface) RmiCodeGeneration.reconnectableStub(MyInterface.class, "context", "localhost", 5000, new ExponentialFallbackPolicy());
+        myInterface = (MyInterface) RmiCodeGeneration.reconnectableStub("remote.MyInterface", new Class[]{MyInterface.class}, "context", "localhost", 5000, new ExponentialFallbackPolicy());
         try {
             myInterface.hello(0);
             fail("an exception should have occurred");
