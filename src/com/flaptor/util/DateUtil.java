4ce0dea82a5cbe56638b2c5eb60ae9755ce28148
martinmassera <martinmassera@a957e277-6143-0410-b0ff-25ec9014f3a9>
2008-03-11 19:24:18 +0000

    added methods to dateutil
    git-svn-id: http://flaptor-util.googlecode.com/svn/trunk@54 a957e277-6143-0410-b0ff-25ec9014f3a9

--- src/com/flaptor/util/DateUtil.java
+++ src/com/flaptor/util/DateUtil.java
@@ -26,13 +26,36 @@ public class DateUtil {
         return (int)(diff / MILLIS_IN_DAY);
     }
     
+    /**
+     * Compares two calendars to determine if they are the same day 
+     * @param cal1
+     * @param cal2
+     * @return 
+     */
     public static boolean isSameDay(Calendar cal1, Calendar cal2) {
         return 
             cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR) &&
             cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR);        
     }
+
+    /**
+     * Returns the canonical day calendar for a given calendar, which is the first millisecond of 
+     * the day (2008/03/07 15:23:32 992ms --> 2008/03/07 0:0:0 0ms)
+     * @param cal
+     * @return 
+     */
     public static Calendar getCanonicalDay(Calendar cal) {
         Calendar ret = new GregorianCalendar(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), 0 ,0 ,0);
         return ret;
+    }
+    /**
+     * Returns the canonical day of the day specified in days from now
+     * @param daysFromToday today = 0, tomorrow = +1, yesterday = -1, same day next week = +7, etc
+     * @return
+     */
+    public static Calendar getCanonicalDayFromToday(int daysFromToday) {
+        Calendar day = new GregorianCalendar();
+        day.add(Calendar.DAY_OF_YEAR, daysFromToday);
+        return getCanonicalDay(day); 
     }
 }
