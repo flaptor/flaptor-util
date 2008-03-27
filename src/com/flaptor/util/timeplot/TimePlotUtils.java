9510bd17eb1020cd72ea055978ff6f6033637a8a
ignacio.perez <ignacio.perez@a957e277-6143-0410-b0ff-25ec9014f3a9>
2008-03-27 16:26:19 +0000

    Completed javadocs
    git-svn-id: http://flaptor-util.googlecode.com/svn/trunk@72 a957e277-6143-0410-b0ff-25ec9014f3a9

--- src/com/flaptor/util/timeplot/TimePlotUtils.java
+++ src/com/flaptor/util/timeplot/TimePlotUtils.java
@@ -4,35 +4,71 @@ import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import com.google.common.collect.Lists;
 
+/**
+ * This class consists exclusively of static methods that can be used
+ * to work with the TimePlot javascript API
+ * 
+ * @author Ignacio Perez (iperez)
+ *
+ */
 public class TimePlotUtils {
-
-    public static String generateInputDate(Map<Date, List<Number>> data, int precisionDecimals, String rowSeparator) {
-        return generateInputDate(data, precisionDecimals, false, rowSeparator);
+    
+    /**
+     * Generate the String input for TimePlots. The source data is a map from dates to numeric values, where the
+     * dates are the time checkpoints and the numbers are the values of the diferent columns to plot.<br> 
+     * The lists of numbers represent the values for each column and hence all entries must have lists with the 
+     * same size and the columns must be in the same order.<br>
+     * The dataset will be generated in the javascript inline format unless a different <code>rowSeparator</code> 
+     * (for instance: a "\n" to output to a data file) is specified.  
+     *
+     * When using this method, the dataset will be generated in the javascript inline format using the "\n" string 
+     * (not an enter) as a separator.   
+     *  
+     * @param source A {@link Map} from {@link Date} to {@link List<Number>} with the values for each checkpoint.  
+     * @param precisionDecimals The number of decimals to preseve for non integer numbers 
+     * @return The String dataset
+     */
+    public static String generateInputDate(Map<Date, List<Number>> source) {
+        return generateInputDate(source, 5, "\\n");
     }
     
-    public static String generateInputDate(Map<Date, List<Number>> data, boolean invertedOrder, String rowSeparator) {
-        return generateInputDate(data, 5, invertedOrder, rowSeparator);
+    /**
+     * Generate the String input for TimePlots. The source data is a map from dates to numeric values, where the
+     * dates are the time checkpoints and the numbers are the values of the diferent columns to plot.<br> 
+     * The lists of numbers represent the values for each column and hence all entries must have lists with the 
+     * same size and the columns must be in the same order.<br>
+     * The dataset will be generated in the javascript inline format unless a different <code>rowSeparator</code> 
+     * (for instance: a "\n" to output to a data file) is specified.<br>
+     * 
+     * When using this method, the non integer values will preserve up to 5 decimals in its string representation.  
+     * 
+     * @param source A {@link Map} from {@link Date} to {@link List<Number>} with the values for each checkpoint.  
+     * @param rowSeparator The string that will be put between rows (depending on the type of output, a different separator can be used) 
+     * @return The String dataset
+     */    public static String generateInputDate(Map<Date, List<Number>> source, String rowSeparator) {
+        return generateInputDate(source, 5, rowSeparator);
     }
-    
-    public static String generateInputDate(Map<Date, List<Number>> data, String rowSeparator) {
-        return generateInputDate(data, 5, false, rowSeparator);
-    }
-    
-    
-    public static String generateInputDate(Map<Date, List<Number>> data, int precisionDecimals, final boolean invertedOrder, String rowSeparator) {
+        
+    /**
+     * Generate the String input for TimePlots. The source data is a map from dates to numeric values, where the
+     * dates are the time checkpoints and the numbers are the values of the diferent columns to plot.<br> 
+     * The lists of numbers represent the values for each column and hence all entries must have lists with the 
+     * same size and the columns must be in the same order.<br>
+     * 
+     * @param source A {@link Map} from {@link Date} to {@link List<Number>} with the values for each checkpoint.  
+     * @param precisionDecimals The number of decimals to preseve for non integer numbers 
+     * @param rowSeparator The string that will be put between rows (depending on the type of output, a different separator can be used) 
+     * @return The String dataset
+     */
+    public static String generateInputDate(Map<Date, List<Number>> source, int precisionDecimals, String rowSeparator) {
         if (precisionDecimals < 0) {
             throw new IllegalArgumentException("Argument precisionDecimals must be 0 or greater.");
-        }
-        
-        if (rowSeparator == null) {
-            rowSeparator = "\\n";
         }
         
         char[] decimals = new char[precisionDecimals];
@@ -42,21 +78,13 @@ public class TimePlotUtils {
         DecimalFormat decimalFormat = new DecimalFormat(pattern);
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         
-        List<Date> dates = Lists.sortedCopy(data.keySet(), new Comparator<Date>() {
-            public int compare(Date date1, Date date2) {
-                int result = date1.compareTo(date2);
-                if (invertedOrder) {
-                    result = - result;
-                }
-                return result;
-            }
-        });
+        List<Date> dates = Lists.sortedCopy(source.keySet());
         
         StringBuffer buffer = new StringBuffer();
         Integer columns = null;
         
         for (Date date : dates) {
-            List<Number> values = data.get(date);
+            List<Number> values = source.get(date);
             int columnsForDate = values.size();
             
             if (columns == null) {
